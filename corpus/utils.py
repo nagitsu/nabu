496d416aafe8daf9f52d3e64044fcfe1dba6c05b
Agustín Azzinnari <aazzinnari@gmail.com>
2015-03-24 20:07:58 -0300

    corpus: provisional fix to timezone management

--- corpus/utils.py
+++ corpus/utils.py
@@ -8,7 +8,9 @@ def custom_encoder(obj):
     """
     if isinstance(obj, datetime):
         epoch = datetime(1970, 1, 1)
-        return (obj - epoch).total_seconds()
+        # TODO: Manage TZ info correctly; we're just removing it for now.
+        since_epoch = obj.replace(tzinfo=None) - epoch.replace(tzinfo=None)
+        return since_epoch.total_seconds()
     else:
         msg = "Object of type {} with value of {} is not JSON serializable"
         raise TypeError(msg.format(type(obj), repr(obj)))

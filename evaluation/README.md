9fea8144458562e30f24d69906e380320835ffd5
Alejandro <alejandro@tryolabs.com>
2015-06-30 01:00:04 -0300

    Evaluation: Added initial evaluation test (w2v-questions-words).

---
+++ evaluation/README.md
@@ -0,0 +1,88 @@
+######: capital-common-countries
+Relaciona países con sus capitales.
+```
+Formato:
+  capital país capital_2 país_2
+```
+
+######: capital-world
+También relaciona países con sus capitales. Aún no se cuál es la diferencia con el anterior.
+```
+Formato:
+  capital país capital_2 país_2
+```
+
+######: currency
+Relaciona monedas con países.
+```
+Formato:
+  país moneda país_2 moneda_2
+```
+
+######: city-in-state
+Vincula ciudades de EEUU y los estados donde se encuentran. No es muy útil para nosotros, los dejamos fuera. Mejor utilizar ciudades no capitales y países de habla hispana?
+```
+Formato:
+  ciudad estado ciudad_2 estado_2
+```
+######: family
+Son relaciones familiares. `Ej: brother sister - dad mom`
+```
+Formato:
+  relacion_M relacion_F relacion_M_2 relacion_F_2
+```
+
+######: gram1-adjective-to-adverb
+Relación adjetivo con adverbio. `Ej: amazing amazingly - free freely`
+```
+Formato:
+  verbo adverbio verbo_2 adverbio_2
+```
+
+######: gram2-opposite
+Relación entre adjetivos y sus opuestos. `Ej: decided undecided likely unlikely`
+```
+Formato:
+  adjetivo opuesto adjetivo_2 opuesto_2
+```
+
+######: gram3-comparative
+Comparativos. No nos sirven es idioma Español, los dejamos fuera.
+
+######: gram4-superlative
+Superlativos. Tampoco nos sirven, los dejamos fuera.
+
+######: gram5-present-participle
+Participios. `Ej: go going say saying`
+```
+Formato:
+  verbo participio verbo_2 participio_2
+```
+
+######: gram6-nationality-adjective
+Nacionalidades. Es interesante pensar que tan bien represntadas estarán estas palabras en nuestro corpus. `Ej: Austria Austrian Mexico Mexican`
+```
+Formato:
+  país nacionalidad país_2 nacionalidad_2
+```
+
+######: gram7-past-tense
+Pretérito simple. Necesitaríamos ejemplos de pretérito imperfecto.
+```
+Formato:
+  verbo pasado verbo_2 pasado_2
+```
+
+######: gram8-plural
+Plurales de sustantivos.
+```
+Formato:
+  sustantivo plural sustantivo_2 plural_2
+```
+
+######: gram9-plural-verbs
+Plurales de verbos.
+```
+Formato:
+  verbo plural verbo_2 plural_2
+```
---
+++ evaluation/translate-questions-words.py
@@ -0,0 +1,154 @@
+import sys
+import locale
+import urllib2
+import cStringIO
+from datetime import datetime
+
+import dataset
+
+# Database.
+DB_NAME = 'w2v-questions-words.db'
+TRANSLATIONS_TABLE = 'translations'
+
+# Files.
+IN_FILE = 'mod-w2v-questions-words.txt'
+OUT_FILE = 'w2v-questions-words-ES.txt'
+
+# Marks.
+DELETE_MARK = 'DELETE_MARK'
+
+
+def create_db():
+    f = open(IN_FILE, 'r')
+    lines = f.readlines()
+
+    total_words = set([])
+    db = dataset.connect('sqlite:///%s' % DB_NAME)
+    translations_table = db[TRANSLATIONS_TABLE]
+
+    for line in lines:
+        line = line.strip()
+        if line.startswith(':'):
+            # Comment line.
+            continue
+
+        # Add words to set.
+        total_words.update(line.split())
+
+    print 'Total words in file:', len(total_words)
+
+    rows = [
+        dict(
+            word=w, translation='', updated=datetime.now()
+        ) for w in total_words
+    ]
+    translations_table.insert_many(rows)
+    translations_table.create_index(['word'])
+
+
+def google_translate(to_translate, to_language="auto", language="auto"):
+    '''
+    Return the translation using google translate you must shortcut the
+    language you define (French = fr, English = en, Spanish = es, etc...) if
+    you don't define anything it will detect it or use english by default.
+
+    Example:
+        > print(translate("salut tu vas bien?", "en"))
+        > hello you alright?
+
+    Taken from:
+        https://github.com/mouuff/Google-Translate-API/blob/master/python/GoogleTranslate.py
+    '''
+    agents = {
+        'User-Agent': "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.1.4322; .NET CLR 2.0.50727; .NET CLR 3.0.04506.30)"
+    }
+    before_trans = 'class="t0">'
+    link = "http://translate.google.com/m?hl=%s&sl=%s&q=%s" % (
+        to_language, language, to_translate.replace(" ", "+")
+    )
+    request = urllib2.Request(link, headers=agents)
+    page = urllib2.urlopen(request).read()
+    result = page[page.find(before_trans) + len(before_trans):]
+    result = result.split("<")[0]
+    return result
+
+
+def translate():
+    db = dataset.connect('sqlite:///%s' % DB_NAME)
+    table = db[TRANSLATIONS_TABLE]
+
+    while True:
+        try:
+            remaining = table.count(translation='')
+            if not remaining:
+                print '\n[INFO] All words translated.'
+                break
+            print '\n[INFO] Remaining items: %d' % table.count(translation='')
+
+            rows = table.find(translation='', _limit=10)
+            for row in rows:
+                google_help = google_translate(
+                    row['word'], to_language='es', language='en'
+                ).decode('utf8')
+
+                question = "\nPlease translate '%s' (Google: '%s'): " % (
+                    row['word'], google_help
+                )
+                trans = raw_input(
+                    question.encode('utf8')
+                ).decode(
+                    sys.stdin.encoding or locale.getpreferredencoding(True)
+                ).strip()
+
+                if trans == '=':
+                    # No need for translation, it is the same in Spanish.
+                    trans = row['word']
+                elif trans in (',', '.'):
+                    # Troublesome word, mark for removal.
+                    trans = DELETE_MARK
+                elif not trans:
+                    # No input, we will use Google's suggestion.
+                    trans = google_help
+
+                print u'You wrote:', trans
+
+                # Update db.
+                table.update(
+                    dict(
+                        word=row['word'], translation=trans,
+                        updated=datetime.now()
+                    ),
+                    ['word']
+                )
+        except (KeyboardInterrupt, SystemExit):
+            print '\nInterrupt received... Closing.'
+            sys.exit()
+
+
+def translate_file():
+    db = dataset.connect('sqlite:///%s' % DB_NAME)
+    trans = {}
+    for row in db[TRANSLATIONS_TABLE]:
+        if row['translation'] != DELETE_MARK:
+            trans[row['word']] = row['translation']
+
+    with open(IN_FILE, 'r') as f:
+        lines = f.readlines()
+
+    buff = cStringIO.StringIO()
+    for line in lines:
+        try:
+            new_line = u' '.join([trans[w] for w in line.split()])
+        except KeyError:
+            # This line has a word for which we have no translation, skip it.
+            continue
+        buff.write('%s\n' % new_line.encode('utf8'))
+
+    with open(OUT_FILE, 'w') as f:
+        lines = f.write(buff.getvalue())
+    buff.close()
+
+if __name__ == '__main__':
+    # create_db()
+    # translate()
+    translate_file()

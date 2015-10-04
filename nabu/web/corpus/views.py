53f5571f793cbbe5d7ab70d82ef655b280a56b28
Agustín Azzinnari <aazzinnari@gmail.com>
2015-10-04 16:39:31 -0300

    web: add corpus search API

--- nabu/web/corpus/views.py
+++ nabu/web/corpus/views.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, jsonify
+from elasticsearch import ElasticsearchException
+from flask import Blueprint, jsonify, request
 
 from nabu.core.index import es
 
@@ -8,10 +9,10 @@ bp = Blueprint('corpus', __name__, url_prefix='/corpus')
 
 def get_corpus_size():
     query = {
-        "aggs": {
-            "words": {
-                "sum": {
-                    "field": "word_count"
+        'aggs': {
+            'words': {
+                'sum': {
+                    'field': 'word_count'
                 }
             }
         }
@@ -25,15 +26,15 @@ def get_corpus_size():
 
 def get_corpus_size_by_source():
     query = {
-        "aggs": {
-            "per_source": {
-                "terms": {
-                    "field": "data_source",
-                    "size": 0,
+        'aggs': {
+            'per_source': {
+                'terms': {
+                    'field': 'data_source',
+                    'size': 0,
                 },
-                "aggs": {
-                    "words": {
-                        "sum": {"field": "word_count"}
+                'aggs': {
+                    'words': {
+                        'sum': {'field': 'word_count'}
                     }
                 }
             }
@@ -59,31 +60,31 @@ def get_corpus_size_by_source():
 
 def get_corpus_size_increase():
     query = {
-        "query": {
-            "range": {
-                "entry.date_scraped": {
-                    "gte": "now-10d/d"
+        'query': {
+            'range': {
+                'entry.date_scraped': {
+                    'gte': 'now-10d/d'
                 }
             }
         },
-        "aggs": {
-            "over_time": {
-                "terms": {
-                    "field": "data_source",
-                    "min_doc_count": 0,
-                    "size": 0,
+        'aggs': {
+            'over_time': {
+                'terms': {
+                    'field': 'data_source',
+                    'min_doc_count': 0,
+                    'size': 0,
                 },
-                "aggs": {
-                    "over_time": {
-                        "date_histogram": {
-                            "field": "entry.date_scraped",
-                            "format": "yyyy-MM-dd",
-                            "interval": "1d",
-                            "min_doc_count": 0,
+                'aggs': {
+                    'over_time': {
+                        'date_histogram': {
+                            'field': 'entry.date_scraped',
+                            'format': 'yyyy-MM-dd',
+                            'interval': '1d',
+                            'min_doc_count': 0,
                         },
-                        "aggs": {
-                            "word_counts": {
-                                "sum": {"field": "word_count"}
+                        'aggs': {
+                            'word_counts': {
+                                'sum': {'field': 'word_count'}
                             }
                         }
                     }
@@ -136,3 +137,66 @@ def stats():
         'by_source': get_corpus_size_by_source(),
         'over_time': get_corpus_size_increase(),
     })
+
+
+def clean_doc(doc):
+    cleaned = {
+        'document_id': doc['_id'],
+        'source': doc['_source']['data_source'],
+        'date_scraped': doc['_source']['entry']['date_scraped'],
+    }
+
+    try:
+        cleaned['snippet'] = doc['highlight']['content'][0]
+    except (KeyError, IndexError):
+        # If no highlighting present, return the full field.
+        cleaned['snippet'] = doc['_source']['content']
+
+    return cleaned
+
+
+@bp.route('/search/', methods=['POST'])
+def search():
+    # TODO: Download link.
+    # TODO: Sanitize the query somehow? Or make less powerful.
+    offset = int(request.args.get('offset', 0))
+    user_query = request.get_json(force=True)['query']
+
+    highlight = {
+        'fields': {
+            'content': {
+                'fragment_size': 100,
+                'number_of_fragments': 1
+            }
+        }
+    }
+
+    query = {
+        'query': user_query,
+        'aggs': {'words': {'sum': {'field': 'word_count'}}},
+        'highlight': highlight,
+        'from': offset,
+        'size': 25,
+    }
+
+    try:
+        response = es.search(index='nabu', doc_type='document', body=query)
+    except ElasticsearchException as e:
+        return jsonify(message=e.error, error='Bad Request'), 400
+
+    word_count = response['aggregations']['words']['value']
+    download_link = 'not-available-yet'
+    hits = [clean_doc(hit) for hit in response['hits']['hits']]
+
+    data = {
+        'word_count': word_count,
+        'download_link': download_link,
+        'hits': hits
+    }
+
+    meta = {
+        'count': response['hits']['total'],
+        'offset': offset,
+    }
+
+    return jsonify(meta=meta, data=data)

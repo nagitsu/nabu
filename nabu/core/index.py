ce9ee5635bca1cfa899c4292110a1fde6d8627e0
Agustín Azzinnari <aazzinnari@gmail.com>
2015-10-07 22:00:38 -0300

    core: move utility functions into its own script

--- nabu/core/index.py
+++ nabu/core/index.py
@@ -1,11 +1,8 @@
 import json
 
 from datetime import datetime
-
 from elasticsearch import Elasticsearch
-from elasticsearch.helpers import bulk
 
-from nabu.core.models import db, DataSource, Document, Entry
 from nabu.corpus import sources
 
 
@@ -122,40 +119,6 @@ def prepare_document(doc):
     })
 
     return payload
-
-
-def prepare_documents(limit=None):
-    """
-    Returns an iterator of ES actions ready to pass to `bulk` of all the
-    documents available on the database.
-    """
-    # Retrieve documents with at least 10 words; the rest shouldn't even be
-    # stored.
-    documents = db.query(Document).join(Entry).join(DataSource)\
-                  .filter(Document.word_count >= 10)\
-                  .yield_per(50000)
-
-    if limit:
-        documents = documents.limit(limit)
-
-    for doc in documents:
-        payload = prepare_document(doc)
-
-        action = {
-            "_op_type": "index",
-            "_index": "nabu",
-            "_type": "document",
-            "_id": doc.id,
-            "_source": payload,
-        }
-
-        yield action
-
-
-def reindex_documents(limit=None, force=False):
-    create_index(force=force)
-    document_iterator = prepare_documents(limit=limit)
-    bulk(es, document_iterator)
 
 
 # Check everything is correctly configured when importing the module.

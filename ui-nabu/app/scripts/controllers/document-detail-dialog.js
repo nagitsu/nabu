738356e2e12e56379c8890c1627770dccb17bb42
Alejandro <alejandro@tryolabs.com>
2015-10-12 20:31:54 -0300

    ui-nabu: added document detail dialog

---
+++ ui-nabu/app/scripts/controllers/document-detail-dialog.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name nabuApp.controller:DocumentDetailDialogCtrl
+ * @description
+ * # DocumentDetailDialogCtrl
+ * Controller of the nabuApp
+ */
+angular.module('nabuApp')
+  .controller('DocumentDetailDialogCtrl', function ($scope, $mdDialog, docData) {
+    $scope.corpusDoc = docData.data;
+
+    $scope.cancel = function() {
+        $mdDialog.cancel();
+    };
+  });

30494e51ea54275098c02251408a3b169002e2ef
Alejandro <alejandro@tryolabs.com>
2015-10-12 20:27:24 -0300

    ui-nabu: added missing data functions to Corpus service

--- ui-nabu/app/scripts/services/corpus.js
+++ ui-nabu/app/scripts/services/corpus.js
@@ -17,6 +17,18 @@ angular.module('nabuApp')
         return Restangular.all('corpus').all('search').post(
           query, {offset: offset}
         );
-      }
+      },
+
+      stats: function () {
+        return Restangular.all('corpus').one('stats').get();
+      },
+
+      documentRetrieve: function (docId) {
+        return Restangular.all('corpus').one('document', docId).get();
+      },
+
+      enums: function () {
+        return Restangular.all('corpus').one('enums').get();
+      },
     };
   });

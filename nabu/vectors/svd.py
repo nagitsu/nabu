9d185be2df454a8f0e6dc900b8c17e21bdc02da8
Agustín Azzinnari <aazzinnari@gmail.com>
2015-10-27 21:19:04 -0300

    vectors: add advanced parameters for SVD

--- nabu/vectors/svd.py
+++ nabu/vectors/svd.py
@@ -1,13 +1,11 @@
 import numpy as np
 import unicodedata
-import random
 import logging
 
 from collections import Counter
-from itertools import islice
-from os.path import join
-from subprocess import Popen, PIPE
 
+from math import sqrt
+from random import random, randint
 from elasticsearch import Elasticsearch
 from elasticsearch.helpers import scan
 from nltk.tokenize import RegexpTokenizer
@@ -43,35 +41,52 @@ def read_corpus():
             yield " ".join(tokens)
 
 
-def build_vocab(corpus):
+def build_vocab(corpus, min_count=10, max_count=None):
     vocab = Counter()
 
     for sentence in corpus:
-        tokens = sentence.split()
-        if len(tokens) < 2:
-            # If there aren't at least two words, discard the sentence.
-            continue
-        vocab.update(Counter(tokens))
+        vocab.update(Counter(sentence.split()))
 
-    # TODO: min_count
-    return dict(vocab)
+    return {k: v for k, v in vocab.most_common(max_count) if v >= min_count}
 
 
-def build_pairs(vocab, corpus, window=5):
+def build_pairs(vocab, corpus, window=5, subsample=10e-5, dynamic_window=True):
     pairs = Counter()
 
-    # TODO: subsampling
-    # TODO: dynamic window
+    # Calculate the probability of removing each word in the vocabulary.
+    # If count <= subsample, the probability will be negative, keep positives
+    # only.
+    corpus_size = sum(vocab.values())
+    subsample = subsample * corpus_size
+    subsampler = {
+        word: 1 - sqrt(subsample / count)
+        for word, count in vocab.items()
+        if count > subsample
+    }
+
     for sentence in corpus:
 
         tokens = [t for t in sentence.split() if t in vocab]
+        if subsample:
+            # If the token is in the list of tokens to subsample, draw a random
+            # number and see if we should keep it.
+            tokens = [
+                t for t in tokens
+                if t not in subsampler or random() > subsampler[t]
+            ]
+
         len_tokens = len(tokens)
 
         for i, token in enumerate(tokens):
-            start = i - window
+            if dynamic_window:
+                curr_win = randint(1, window)
+            else:
+                curr_win = window
+
+            start = i - curr_win
             if start < 0:
                 start = 0
-            end = i + window + 1
+            end = i + curr_win + 1
             if end > len_tokens:
                 end = len_tokens
 
@@ -106,15 +121,16 @@ def build_cooccurrence(vocab, pairs):
 
     counts = counts + tmp_counts.tocsr()
 
-    return counts, words
+    return counts, words, w2i
 
 
-def build_ppmi(cooccur):
-    # TODO: cds
-
-    # TODO: sum_w and sum_c have some zero entries; why?
+def build_ppmi(cooccur, cds=0.75):
+    # TODO: What to do with words that don't appear in any pair?
     sum_w = np.array(cooccur.sum(axis=1))[:, 0]
     sum_c = np.array(cooccur.sum(axis=0))[0, :]
+    if cds:
+        sum_c = sum_c ** cds
+    # TODO: Is this OK? Should the sum be done with the cds'ed counts?
     sum_total = sum_c.sum()
 
     sum_w = np.reciprocal(sum_w)
@@ -150,22 +166,46 @@ def multiply_by_columns(matrix, col_coefs):
     return matrix.dot(normalizer.tocsr())
 
 
-def build_svd(ppmi, dim=100):
+def build_svd(ppmi, dim=100, sum_context=True, eig_weight=0, normalize=True):
     ut, s, vt = sparsesvd(ppmi.tocsc(), dim)
-    return ut, s, vt
+
+    u = np.dot(ut.T, np.diag(s ** eig_weight))
+    if sum_context:
+        u += vt.T
+
+    if normalize:
+        norm = np.linalg.norm(u, axis=1)
+        u = u / norm.reshape(-1, 1)
+
+    return u
 
 
 def main():
+    min_count = 10
+    max_count = None
+    dim = 100
+    window = 5
+    subsample = 10e-5
+    sum_context = True
+
     logger.debug('building vocabulary from corpus')
-    vocab = build_vocab(read_corpus())
+    vocab = build_vocab(
+        read_corpus(),
+        min_count=min_count,
+        max_count=max_count,
+    )
     logger.debug('%s words found', len(vocab))
 
     logger.debug('building the pair counts')
-    pairs = build_pairs(vocab, read_corpus())
+    pairs = build_pairs(
+        vocab, read_corpus(),
+        window=window,
+        subsample=subsample,
+    )
     logger.debug('%s pairs generated', len(pairs))
 
     logger.debug('building the cooccurrence matrix')
-    cooccur, words = build_cooccurrence(vocab, pairs)
+    cooccur, words, w2i = build_cooccurrence(vocab, pairs)
     logger.debug('%s non-zero entries', cooccur.nnz)
 
     logger.debug('building the explicit PPMI matrix of the coocc matrix')
@@ -173,8 +213,15 @@ def main():
     logger.debug('%s non-zero entries', ppmi.nnz)
 
     logger.debug('building the SVD representation of the PPMI matrix')
-    ut, s, vt = build_svd(ppmi)
+    u = build_svd(ppmi, dim=dim, sum_context=sum_context)
     logger.debug('SVD representation obtained')
+
+    # Sanity check.
+    vec = u[w2i['uruguay']]
+    dists = np.dot(u, vec) / np.linalg.norm(u, axis=1) / np.linalg.norm(vec)
+    word_ids = np.argsort(-dists)
+    for idx in word_ids[:15]:
+        print(words[idx], dists[idx])
 
     from IPython import embed
     embed(display_banner=False)

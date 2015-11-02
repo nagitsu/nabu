4f520f0d990435288adb1cdc246bea7bbdcb3be2
Alejandro <alejandro@tryolabs.com>
2015-11-01 21:31:16 -0300

    ui-nabu: tied 'new embedding' view with 'embeddings' view

--- ui-nabu/app/scripts/controllers/embeddings.js
+++ ui-nabu/app/scripts/controllers/embeddings.js
@@ -10,7 +10,29 @@
  * Controller of the nabuApp
  */
 angular.module('nabuApp')
-  .controller('EmbeddingsCtrl', function ($scope, Embeddings, embeddingList) {
+  .controller('EmbeddingsCtrl', function ($scope, $mdDialog, Enums, Embeddings, embeddingList) {
+    $scope.dialogLoading = false;
     $scope.embeddings = embeddingList;
+
+    $scope.newEmbeddingDialog = function(ev) {
+        if ($scope.dialogLoading) {
+            return;
+        }
+        $scope.dialogLoading = true;
+        $mdDialog.show({
+            controller: 'EmbeddingNewDialogCtrl',
+            templateUrl: 'views/embedding-new.html',
+            targetEvent: ev,
+            clickOutsideToClose: true,
+            resolve: {
+                modelEnums: function(Enums) {
+                    return Enums.models();
+                }
+            },
+            onComplete: function() {
+                $scope.dialogLoading = false;
+            }
+        });
+    };
   });
 })(angular);

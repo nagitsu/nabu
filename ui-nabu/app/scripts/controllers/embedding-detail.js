a6f5eebf09a398ac7f13a6dcc2f29b1090b749fc
Alejandro <alejandro@tryolabs.com>
2015-12-10 01:49:59 -0300

    ui-nabu: initial version of embedding-detail view.

--- ui-nabu/app/scripts/controllers/embedding-detail.js
+++ ui-nabu/app/scripts/controllers/embedding-detail.js
@@ -10,7 +10,22 @@
  * Controller of the nabuApp
  */
 angular.module('nabuApp')
-  .controller('EmbeddingDetailCtrl', function ($scope, Embeddings, embedding) {
+  .controller('EmbeddingDetailCtrl', function ($scope, $state, VerifyDelete, Embeddings, embedding) {
     $scope.embedding = embedding;
+
+    $scope.searchQuery = angular.toJson($scope.embedding.corpus.query, true);
+    $scope.aceLoaded = function(_editor) {
+        // Options
+        _editor.setReadOnly(true);
+    };
+
+    $scope.deleteEmbedding = function() {
+        var msg = 'Are you sure you want to delete this embedding?';
+        new VerifyDelete(msg, 'Embedding').then(function() {
+            Embeddings.delete($scope.embedding.id).then(function() {
+                $state.go("initial.tabs.embeddings");
+            });
+        });
+    };
   });
 })(angular);

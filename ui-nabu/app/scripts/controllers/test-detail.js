c4d58ed3da0c5160a38102ff1c70774b33159f82
Alejandro <alejandro@tryolabs.com>
2016-01-07 02:41:12 -0300

    ui-nabu: updated test-detail view

--- ui-nabu/app/scripts/controllers/test-detail.js
+++ ui-nabu/app/scripts/controllers/test-detail.js
@@ -10,8 +10,29 @@
  * Controller of the nabuApp
  */
 angular.module('nabuApp')
-  .controller('TestDetailCtrl', function ($scope, $state, TestSets, testset) {
+  .controller('TestDetailCtrl', function (
+    $scope, $state, VerifyDelete, TestSets, testset
+  ) {
     $scope.testSet = testset;
-    console.log($scope.testSet);
+
+    $scope.updateTestSet = function() {
+        // In this case we can only update description and name attributes.
+        var editTest = {
+            "name": $scope.testSet.name,
+            "description": $scope.testSet.description
+        };
+        TestSets.update($scope.testSet.id, editTest).then(function(test) {
+            $scope.testSet = test.data;
+        });
+    };
+
+    $scope.deleteTestSet = function() {
+        var msg = 'Are you sure you want to delete this test set?';
+        new VerifyDelete(msg, 'Test Set').then(function() {
+            TestSets.delete($scope.testSet.id).then(function() {
+                $state.go("initial.tabs.tests");
+            });
+        });
+    };
   });
 })(angular);

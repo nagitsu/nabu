3a06f3e2d28b75494c7e194ba1e8470fce872464
Agustín Azzinnari <aazzinnari@gmail.com>
2016-03-03 18:37:31 -0300

    corpus: use the new index, not the old one

--- nabu/core/settings.py
+++ nabu/core/settings.py
@@ -4,6 +4,7 @@ LOG_FILE_LOCATION = '/home/nabu/logs/nabu.log'
 ES_HOST = 'golbat.ydns.eu'
 ES_HTTP_AUTH = ('nabu', 'nabunabu')
 ES_INDEX = 'nabu'
+ES_INDEX = 'nabu2'
 ES_DOCTYPE = 'document'
 
 BROKER_URL = 'redis://localhost:6379/0'
